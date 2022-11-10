068dd48a2e1536f69fc77e88390ee110db5bfaf1
Chaitanya Jariwala <jariwalc@uwindsor.ca>
2022-11-10 15:10:18 -0500

    testing

---
+++ src/main/java/com/trigonometry_library/App.java
@@ -0,0 +1,25 @@
+package com.trigonometry_library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    // public static void main( String[] args )
+    // {
+    //     System.out.println( "Hello World!" );
+    // }
+
+    public double sin (double x) {
+        return Math.sin(x);
+    }
+
+    public double cos (double x) {
+        return Math.cos(x);
+    }
+
+    public double tan (double x) {
+        return Math.tan(x);
+    }
+}
